2ee89320a3db9fd9c4f3cfc22ea745b52b632d07
Sumit <sumit.raghuwanshi192@gmail.com>
2023-03-30 14:49:43 +0530

    Add Rubocop for code-style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
@@ -5,10 +7,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
   after_create :assign_default_role
 
   def assign_default_role
-    self.add_role(:user) if self.roles.blank?
+    add_role(:user) if roles.blank?
   end
 end
