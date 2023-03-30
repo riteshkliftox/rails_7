4c6db2e69f9c22bea963830f985807bbda13b6ed
Sumit <sumit.raghuwanshi192@gmail.com>
2023-03-30 16:21:29 +0530

    Added bootstrap form and alret classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    case flash_type.to_sym
+    when :error then 'danger'
+    when :alert then 'warning'
+    when :notice then 'info'
+    else
+      flash_type.to_s
+    end
+  end
 end
